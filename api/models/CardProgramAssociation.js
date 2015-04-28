80ac54e81cd9838748d0ef7bb35a6db2fb404ea5
certainplotservices <contact@certainplotservices.com>
2015-04-28 01:52:00 -0400

    added statuses, association and owner to common

---
+++ api/models/CardProgramAssociation.js
@@ -0,0 +1,27 @@
+/**
+ * CardProgramAssociation.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+    autoPK: false,
+
+    attributes: {
+        code: {
+            type: 'string',
+            size: 4,
+            required: true,
+            unique: true,
+            primaryKey: true
+        },
+        name: {
+            type: 'string',
+            required: true
+        }
+    }
+
+};
+
