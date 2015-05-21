6dbcdd743935ac0bdfd67d70e766583133ddf97e
Cliff Vick <cliff@cliffvick.net>
2015-05-20 22:43:23 -0400

    allow for /config/mocks.js as well as adding a path to a mock definition file

--- api/services/Service.js
+++ api/services/Service.js
@@ -1,79 +1,9 @@
-var Promise = require('bluebird');
 var discovery = require('../../lib/discovery.js');
-var request = require('../../lib/request.js');
-
-var serviceCall = function(serviceName) {
-    var service = discovery.getService(serviceName);
-
-    var serviceNotAvailable = function() {
-        return Promise.reject('Service ' + serviceName + ' is not available.');
-    }
-    return {
-        get   : function (path) {
-            if (service) {
-                return request({
-                    method: 'GET',
-                    host: service.ip,
-                    port: service.port,
-                    path: path
-                });
-            }
-            else {
-                return serviceNotAvailable();
-            }
-        },
-        put   : function (path, data) {
-            if (service) {
-                return request({
-                    method: 'PUT',
-                    host: service.ip,
-                    port: service.port,
-                    path: path,
-                    data: data
-                });
-            }
-            else {
-                return serviceNotAvailable();
-            }
-        },
-        post  : function (path, data) {
-            if (service) {
-                return request({
-                    method: 'POST',
-                    host: service.ip,
-                    port: service.port,
-                    path: path,
-                    data: data
-                });
-            }
-            else {
-                return serviceNotAvailable();
-            }
-        },
-        delete: function (path) {
-            if (service) {
-                return request({
-                    method: 'DELETE',
-                    host: service.ip,
-                    port: service.port,
-                    path: path
-                });
-            }
-            else {
-                return serviceNotAvailable();
-            }
-        }
-    }
-}
 
 module.exports = {
-    register: function(serviceOptions) {
-        return discovery.register(serviceOptions);
-    },
+    register: discovery.register,
 
-    request: function(serviceName) {
-        return serviceCall(serviceName);
-    },
+    request: discovery.request,
 
     mocks: {
         add: discovery.addMock
