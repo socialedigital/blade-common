46540ba9df18327afd494267c96f1d4c6c024406
Cliff Vick <cliff@cliffvick.net>
2015-10-20 08:25:44 -0400

    added http status codes to error objects log payload on incoming requests in the http middleware changed logging of discovery announcements to silly so that they won't be logged at 'verbose' level

--- api/services/errors/clientError/ImATeapot.js
+++ api/services/errors/clientError/ImATeapot.js
@@ -5,6 +5,7 @@ function ImATeapot(serviceName, request, response, message) {
     clientError.apply(this, [serviceName, request, response]);
     this.service.response.message = "I'm a Teapot";
     this.name = "I AM A TEAPOT";
+    this.status = 418;
 }
 
 util.inherits(ImATeapot, clientError);
