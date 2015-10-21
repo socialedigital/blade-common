5d40e6df514d41a115a202b41911cf444394f399
certainplotservices <contact@certainplotservices.com>
2015-10-21 10:51:54 -0400

    change config/blade to use contact@mybladecard.com as the from email in message service

--- config/blade.js
+++ config/blade.js
@@ -4,5 +4,5 @@ module.exports.blade = {
     inactivityTimeout: (60 * 10),  // how long to block if fail password 3 times
     blockedEmailTimeout: (60 * 15),
     twoFactorCodeTimeout: (60 * 2),
-    sesSentFromEmail: 'contact@certainplotservices.com'
+    sesSentFromEmail: 'contact@mybladecard.com'
 };
